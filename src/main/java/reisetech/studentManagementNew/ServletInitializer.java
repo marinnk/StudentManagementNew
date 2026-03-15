996953bfd26149f8485a4f58049769daf7427c38
marinnk <marinnobaka@yahoo.co.jp>
2026-03-15 13:09:41 +0900

    intelliJで作成したコードを追加

---
+++ src/main/java/reisetech/studentManagementNew/ServletInitializer.java
@@ -0,0 +1,13 @@
+package reisetech.studentManagementNew;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(StudentManagementNewApplication.class);
+	}
+
+}
---
+++ src/test/java/reisetech/studentManagementNew/StudentManagementNewApplicationTests.java
@@ -0,0 +1,13 @@
+package reisetech.studentManagementNew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentManagementNewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
