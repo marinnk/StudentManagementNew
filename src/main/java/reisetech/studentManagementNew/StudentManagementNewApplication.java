7dc94a32e406aa31fa3db2734ebedbe688d6de23
marinnk <marinnobaka@yahoo.co.jp>
2026-03-18 21:37:25 +0900

    GetMappingとPostMappingの動作確認

--- src/main/java/reisetech/studentManagementNew/StudentManagementNewApplication.java
+++ src/main/java/reisetech/studentManagementNew/StudentManagementNewApplication.java
@@ -1,21 +1,39 @@
 package reisetech.studentManagementNew;
 
+import java.util.Map;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class StudentManagementNewApplication {
 
+	private String name = "Enami Koji";
+	public String age = "32";
+
+
 	public static void main(String[] args) {
 		SpringApplication.run(StudentManagementNewApplication.class, args);
 	}
 
-	@GetMapping("/hello")
-	public String hello() {
-		return "Hello, World!";
+	@GetMapping("/studentInfo")
+	public String getstudentInfo() {
+		return name + " " + age + "歳";
 	}
+
+	@PostMapping("/studentInfo")
+	public void setstudentInfo(String name, String age) {
+		this.name = name;
+		this.age = age;
+	}
+
+	@PostMapping("/studentName")
+	public void updateStudentName(String name) {
+		this.name = name;
+	}
+
 
 }
