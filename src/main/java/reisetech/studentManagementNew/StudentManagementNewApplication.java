3b106d97e32dc6a994be2e8459a71514ff7a9df8
marinnk <marinnobaka@yahoo.co.jp>
2026-03-21 21:00:08 +0900

    application.propartiesのデータベース名修正、DBから情報取得

--- src/main/java/reisetech/studentManagementNew/StudentManagementNewApplication.java
+++ src/main/java/reisetech/studentManagementNew/StudentManagementNewApplication.java
@@ -1,21 +1,40 @@
 package reisetech.studentManagementNew;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class StudentManagementNewApplication {
 
+	@Autowired
+	private StudentRepository repository;
+
+	private String name = "Enami Koji";
+	public String age = "32";
+
 	public static void main(String[] args) {
 		SpringApplication.run(StudentManagementNewApplication.class, args);
 	}
 
-	@GetMapping("/hello")
-	public String hello() {
-		return "Hello, World!";
+	@GetMapping("/studentInfo")
+	public String getstudentInfo() {
+		Student student = repository.searchByName("Ramuda Amemura");
+		return student.getName() + " " + student.getAge() + "歳";
 	}
 
+	@PostMapping("/studentInfo")
+	public void setstudentInfo() {
+		this.name = name;
+		this.age = age;
+	}
+
+	@PostMapping("/studentName")
+	public void updateStudentName(String name) {
+		this.name = name;
+	}
 }
