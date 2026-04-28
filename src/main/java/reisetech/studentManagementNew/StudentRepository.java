48b12409ff08eeccd09f3d0fb67d506c99b6cc2d
marinnk <marinnobaka@yahoo.co.jp>
2026-04-28 19:31:59 +0900

    使わない登録、更新、削除のRepository削除

--- src/main/java/reisetech/studentManagementNew/StudentRepository.java
+++ src/main/java/reisetech/studentManagementNew/StudentRepository.java
@@ -13,12 +13,4 @@ public interface StudentRepository {
   @Select("SELECT * FROM student")
   List<Student> getAllStudents();
 
-  @Insert("INSERT student values(#{name}, #{age})")
-  void registerStudent(String name, int age);
-
-  @Update("UPDATE student SET age= #{age} WHERE name = #{name}")
-  void updateStudent(String name,int age);
-
-  @Delete("DELETE FROM student WHERE name = #{name}")
-  void deleteStudent(String name);
 }
