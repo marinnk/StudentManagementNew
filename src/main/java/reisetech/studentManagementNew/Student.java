c4e7f8d318073b24e32756c050891de2b6aa875d
marinnk <marinnobaka@yahoo.co.jp>
2026-04-30 21:18:42 +0900

    追加

--- src/main/java/reisetech/studentManagementNew/Student.java
+++ src/main/java/reisetech/studentManagementNew/Student.java
@@ -1,22 +1,12 @@
 package reisetech.studentManagementNew;
 
 public class Student {
+  private String id;
   private String name;
+  private String furigana;
+  private String nickname;
+  private String mail;
+  private String place;
   private int age;
-
-  public String getName() {
-    return name;
-  }
-
-  public void setName(String name) {
-    this.name = name;
-  }
-
-  public int getAge() {
-    return age;
-  }
-
-  public void setAge(int age) {
-    this.age = age;
-  }
+  private String gender;
 }
