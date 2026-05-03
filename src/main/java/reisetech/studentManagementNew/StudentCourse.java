5b31cee4f5d0d5f3ea8f5263d344b00815906454
marinnk <marinnobaka@yahoo.co.jp>
2026-05-03 18:09:16 +0900

    パッケージ追加

--- src/main/java/reisetech/studentManagementNew/StudentCourse.java
+++ src/main/java/reisetech/studentManagementNew/StudentCourse.java
@@ -1,5 +1,12 @@
-import java.time.LocalDateTime;
+package reisetech.studentManagementNew;
 
+import java.time.LocalDateTime;
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Getter
+@Setter
 public class StudentCourse {
     private String id;
     private String studentId;
