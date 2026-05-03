53cfdd4a687bd4ba9413a0090eec41e13005a404
marinnk <marinnobaka@yahoo.co.jp>
2026-05-03 16:47:37 +0900

    クラス作成と定義追加

---
+++ src/main/java/StudentCourse.java
@@ -0,0 +1,10 @@
+import java.time.LocalDateTime;
+
+public class StudentCourse {
+    private String id;
+    private String studentId;
+    private String CourseName;
+    private LocalDateTime start;
+    private LocalDateTime end;
+
+}
