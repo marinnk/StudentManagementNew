2f0b2411c0817c475dc7768ea95f42a449a420d3
marinnk <marinnobaka@yahoo.co.jp>
2026-05-07 21:36:14 +0900

    Controller作成

--- src/main/java/reisetech/studentManagementNew/StudentManagementNewApplication.java
+++ src/main/java/reisetech/studentManagementNew/StudentManagementNewApplication.java
@@ -1,22 +1,13 @@
 package reisetech.studentManagementNew;
 
-import java.util.List;
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-import reisetech.studentManagementNew.repository.StudentRepository;
-import reisetech.studentManagementNew.data.Student;
-import reisetech.studentManagementNew.data.StudentCourse;
+
 
 
 @SpringBootApplication
-@RestController
 public class StudentManagementNewApplication {
-
-	@Autowired
-	private StudentRepository repository;
 
 
 	public static void main(String[] args) {
--- src/main/java/reisetech/studentManagementNew/controller/StudentController.java
+++ src/main/java/reisetech/studentManagementNew/controller/StudentController.java
@@ -1,5 +1,32 @@
 package reisetech.studentManagementNew.controller;
 
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reisetech.studentManagementNew.data.Student;
+import reisetech.studentManagementNew.data.StudentCourse;
+import reisetech.studentManagementNew.service.StudentService;
+
+@RestController
 public class StudentController {
+
+  private StudentService service;
+
+  @Autowired
+  public StudentController(StudentService service) {
+    this.service = service;
+  }
+
+
+  @GetMapping("/student")
+  public List<Student> getStudentList() {
+    return service.searchStudentList();
+  }
+
+  @GetMapping("/studentCourse")
+  public List<StudentCourse> getStudentCourseList() {
+    return service.searchStudentCourseList();
+  }
 
 }
