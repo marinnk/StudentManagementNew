8f0e201131c537e164ac677029b7cbcd3f60b4cc
marinnk <marinnobaka@yahoo.co.jp>
2026-06-02 08:50:17 +0900

    model作成

--- src/main/java/reisetech/studentManagementNew/controller/StudentController.java
+++ src/main/java/reisetech/studentManagementNew/controller/StudentController.java
@@ -2,26 +2,34 @@ package reisetech.studentManagementNew.controller;
 
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import reisetech.studentManagementNew.controller.converter.StudentConverter;
 import reisetech.studentManagementNew.data.Student;
 import reisetech.studentManagementNew.data.StudentCourses;
 import reisetech.studentManagementNew.service.StudentService;
+import org.springframework.ui.Model;
 
-@RestController
+@Controller
 public class StudentController {
 
   private StudentService service;
+  private StudentConverter converter;
 
   @Autowired
-  public StudentController(StudentService service) {
+  public StudentController(StudentService service, StudentConverter converter) {
     this.service = service;
+    this.converter = converter;
   }
 
 
   @GetMapping("/studentList")
-  public List<Student> getStudentList() {
-    return service.searchStudentList();
+  public String getStudentList(Model model) {
+    List<Student> students = service.searchStudentList();
+    List<StudentCourses> studentCourses = service.searchStudentCourseList();
+
+    model.addAttribute("studentList", converter.convertStudentDetails(students, studentCourses));
+    return "studentList";
   }
 
   @GetMapping("/studentCourseList")
