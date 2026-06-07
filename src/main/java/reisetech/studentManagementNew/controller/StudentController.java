7d8e1c3c03ba74c85022cf5e7eb631574d3660ed
marinnk <marinnobaka@yahoo.co.jp>
2026-06-08 08:40:20 +0900

    POSt処理記述

--- src/main/java/reisetech/studentManagementNew/controller/StudentController.java
+++ src/main/java/reisetech/studentManagementNew/controller/StudentController.java
@@ -3,6 +3,7 @@ package reisetech.studentManagementNew.controller;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -40,9 +41,20 @@ public class StudentController {
     return service.searchStudentCourseList();
   }
 
-  @PostMapping("/registerStudent")
-  public String registerStudent(@ModelAttribute StudentDetail studentDetail) {
+  @GetMapping("/newStudent")
+  public String newStudent(Model model) {
+    model.addAttribute("studentDetail", new StudentDetail());
+    return "registerStudent";
+  }
 
+  @PostMapping("/registerStudent")
+  public String registerStudent(@ModelAttribute StudentDetail studentDetail, BindingResult result) {
+    if(result.hasErrors()) {
+      return "registerStudent";
+    }
+
+
+    return "redirect:/studentList";
   }
 
 }
