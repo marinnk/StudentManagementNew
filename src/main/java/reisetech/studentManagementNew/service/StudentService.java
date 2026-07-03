89336a7c847df9b68c92bd1b3e0519abcaba02f0
こうちまりん <marink@kouchimarinnoMacBook-Air.local>
2026-07-03 22:21:33 +0900

    コース情報登録のコード修正

--- src/main/java/reisetech/studentManagementNew/service/StudentService.java
+++ src/main/java/reisetech/studentManagementNew/service/StudentService.java
@@ -1,11 +1,11 @@
 package reisetech.studentManagementNew.service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import reisetech.studentManagementNew.data.Student;
-import reisetech.studentManagementNew.data.StudentCourses;
 import reisetech.studentManagementNew.data.StudentCourses;
 import reisetech.studentManagementNew.domain.StudentDetail;
 import reisetech.studentManagementNew.repository.StudentRepository;
@@ -30,7 +30,12 @@ public class StudentService {
   @Transactional
   public void registerStudent(StudentDetail studentDetail) {
     repository.registerStudent(studentDetail);
-    repository.registerStudentCourse(studentDetail);
+    for(StudentCourses course : studentDetail.getStudentCourses()) {
+      course.setStudentId(studentDetail.getStudent().getId());
+      course.setStart(LocalDateTime.now());
+
+      repository.registerStudentCourse(course);
+    }
   }
 
 }
